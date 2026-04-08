1f5016a2aa455c1624d79ace4b5d377b81fa03b8
septianbeneran <m.septiannn@gmail.com>
2026-04-08 17:36:48 +0700

    TASK: optimize import

--- api/pokemon/src/main/java/dev/septianbeneran/technicaltest/api/pokemon/data/remote/service/PokemonApiRemoteDataSourceImpl.kt
+++ api/pokemon/src/main/java/dev/septianbeneran/technicaltest/api/pokemon/data/remote/service/PokemonApiRemoteDataSourceImpl.kt
@@ -1,10 +1,7 @@
 package dev.septianbeneran.technicaltest.api.pokemon.data.remote.service
 
 import dev.septianbeneran.technicaltest.api.pokemon.data.remote.api.PokemonApi
-import dev.septianbeneran.technicaltest.api.pokemon.data.remote.dto.PokemonDto
-import dev.septianbeneran.technicaltest.api.pokemon.data.remote.dto.PokemonResponse
 import dev.septianbeneran.technicaltest.core.base.BaseDataSource
-import dev.septianbeneran.technicaltest.core.entity.remote.ApiResult
 import javax.inject.Inject
 
 class PokemonApiRemoteDataSourceImpl @Inject constructor(
--- api/pokemon/src/main/java/dev/septianbeneran/technicaltest/api/pokemon/repository/PokemonRepositoryImpl.kt
+++ api/pokemon/src/main/java/dev/septianbeneran/technicaltest/api/pokemon/repository/PokemonRepositoryImpl.kt
@@ -1,13 +1,9 @@
 package dev.septianbeneran.technicaltest.api.pokemon.repository
 
-import dev.septianbeneran.technicaltest.api.pokemon.data.remote.dto.PokemonDto
-import dev.septianbeneran.technicaltest.api.pokemon.data.remote.dto.PokemonResponse
 import dev.septianbeneran.technicaltest.api.pokemon.data.remote.service.PokemonApiRemoteDataSource
 import dev.septianbeneran.technicaltest.core.base.BaseRepository
-import dev.septianbeneran.technicaltest.core.entity.remote.ApiResult
 import dev.septianbeneran.technicaltest.core.util.CoroutineDispatcherProvider
 import dev.septianbeneran.technicaltest.core.util.resultFlow
-import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class PokemonRepositoryImpl @Inject constructor(
